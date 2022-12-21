ed498a83f8dabe1ac034a39cc244b56c29567a4e
qiuzhanghua <qiuzhanghua@icloud.com>
2022-12-21 16:20:02 +0800

    impl M <-> N for users and roles

--- src/main/kotlin/com/example/korm/DemoApplication.kt
+++ src/main/kotlin/com/example/korm/DemoApplication.kt
@@ -1,5 +1,11 @@
 package com.example.korm
 
+
+import org.jetbrains.exposed.dao.LongEntity
+import org.jetbrains.exposed.dao.LongEntityClass
+import org.jetbrains.exposed.dao.UUIDEntity
+import org.jetbrains.exposed.dao.UUIDEntityClass
+import org.jetbrains.exposed.dao.id.EntityID
 import org.jetbrains.exposed.dao.id.LongIdTable
 import org.jetbrains.exposed.dao.id.UUIDTable
 import org.jetbrains.exposed.sql.*
@@ -8,6 +14,7 @@ import org.springframework.boot.CommandLineRunner
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.context.annotation.Bean
+import java.util.UUID
 import javax.sql.DataSource
 
 fun main(args: Array<String>) {
@@ -21,31 +28,75 @@ class DemoApplication {
 //        println(ds.connection)
         Database.connect(ds)
         transaction {
-            SchemaUtils.create(Users, Roles)
+            SchemaUtils.create(Users, Roles, UsersRoles)
             val uid = Users.insertAndGetId {
                 it[name] = "Daniel"
             }
             commit()
             println(uid.value)
             val rid = Roles.insertAndGetId {
-                it[name] = "Admin"
+                it[name] = "User"
             }
             commit()
             println(rid.value)
 
+            val rid2 = Roles.insertAndGetId {
+                it[name] = "Admin"
+            }
+            commit()
+            println(rid2.value)
+            UsersRoles.insert {
+                it[user] = uid
+                it[role] = rid
+            }
+            UsersRoles.insert {
+                it[user] = uid
+                it[role] = rid2
+            }
+            commit()
+
+            User.findById(uid)?.roles?.forEach {
+                println(it.name)
+            }
         }
 
         transaction {
-            println(Users.selectAll().count())
+            println("count of user user <=> role = " + UsersRoles.selectAll().count())
+            UsersRoles.selectAll().filter { it[UsersRoles.role].value == 1.toLong() }.forEach {
+//            UsersRoles.selectAll().forEach {
+                val u = it[UsersRoles.user].value.toString()
+                val r = it[UsersRoles.role].value.toString()
+                println("user = $u, role = $r")
+            }
         }
+
     }
 }
 
-object Users : UUIDTable() {
+object Users : UUIDTable("users") {
     val name = varchar("name", 50)
 }
 
-object Roles: LongIdTable() {
+object Roles: LongIdTable("roles") {
     val name = varchar("name", 100)
 }
 
+object UsersRoles: Table("user_role") {
+    val user = reference("user_id", Users)
+    val role = reference("role_id", Roles)
+    override val primaryKey = PrimaryKey(
+        user, role,
+        name = "PK_USER_ROLE")
+}
+
+class User(id: EntityID<UUID>): UUIDEntity(id) {
+    companion object: UUIDEntityClass<User>(Users)
+    var name by Roles.name
+    var roles by Role via UsersRoles
+}
+
+class Role(id: EntityID<Long>): LongEntity(id) {
+    companion object: LongEntityClass<Role>(Roles)
+    var name by Roles.name
+    var users by User via UsersRoles
+}
